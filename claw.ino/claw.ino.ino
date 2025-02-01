e1d49b20f2bcd44ca84e2a2e6c6a64f46023a6ea
maximilian.von-arnim@isae-supaero.fr <maximilian.von-arnim@isae-supaero.fr>
2025-02-01 20:26:09 +0100

    Added I2C with callback

--- claw.ino/claw.ino.ino
+++ claw.ino/claw.ino.ino
@@ -5,18 +5,24 @@
 // TODO test with hardware
 
 #include "../ACS712/ACS712.h"
+#include <Wire.h> // for i2c ...
 
 ACS712 ACS(A0, 5.0, 1023, 100);
+
+int mA;
 
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(115200);
-  while (!Serial)
-    ;
-  Serial.println(__FILE__);
-  Serial.print("ACS712_LIB_VERSION: ");
-  Serial.println(ACS712_LIB_VERSION);
+  int i2caddr = 0x01;
+  Wire.begin(i2caddr);          // join i2c bus (address optional for master)
+  Wire.onRequest(requestEvent); // register event
+  // Serial.begin(115200);
+  // while (!Serial)
+  //  ;
+  // Serial.println(__FILE__);
+  // Serial.print("ACS712_LIB_VERSION: ");
+  // Serial.println(ACS712_LIB_VERSION);
 
   ACS.autoMidPoint();
   //  Serial.println(ACS.getMidPoint());
@@ -24,8 +30,21 @@ void setup()
 
 void loop()
 {
+  int samplingrate = 100;
   // put your main code here, to run repeatedly:
-  int mA = ACS.mA_DC();
-  Serial.println(mA);
-  delay(100);
+  mA = ACS.mA_DC();
+  // Serial.println(mA);
+  delay(samplingrate);
+}
+
+// function that executes whenever data is requested by master
+
+// this function is registered as an event, see setup()
+
+void requestEvent()
+{
+
+  Wire.write(mA); // respond with message of 6 bytes
+
+  // as expected by master
 }
